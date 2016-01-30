dfa723158f73450ce56dcb6e481ef0f328c0f54e
Derek Bliss <dbliss@fullsail.com>
2016-01-29 20:45:30 -0500

    Added pick option for touch interfaces.

--- Assets/Scripts/MousePick.cs
+++ Assets/Scripts/MousePick.cs
@@ -20,4 +20,20 @@ public static class MousePick {
         return returnObject;
     }
 
+    /// <summary>
+    /// Obtains a game object that is under a touched screen position, or null if none found.
+    /// </summary>
+    /// <returns>null if no object under the touch point, otherwise a game object</returns>
+    public static GameObject GetObjectTouch()
+    {
+        GameObject returnObject = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000.0f))
+        {
+            returnObject = hit.transform.gameObject;
+        }
+        return returnObject;
+    }
+
 }
