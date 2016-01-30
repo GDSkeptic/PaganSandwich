3fd5b1de391c12cccbcbb55be0fa5fae843eee46
varun <avarunsrisrimal@gmail.com>
2016-01-29 22:37:40 -0500

    Added new materials ( basic color) On click sets the obj position to slingshot obj

---
+++ Assets/Scripts/Ingredient.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ingredient : MonoBehaviour
+{
+    public GameObject sling;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject g = MousePick.GetObject();
+            if (g != null)
+            {
+                if (g.tag == gameObject.tag)
+                {
+                    // gameObject.transform.position = sling.transform.position;
+                    Vector3 vec = sling.transform.position;
+                    vec.y += 2.0f;
+                    gameObject.transform.position = vec;
+                    gameObject.transform.parent = sling.transform;
+                }
+            }
+        }
+    }
+}
