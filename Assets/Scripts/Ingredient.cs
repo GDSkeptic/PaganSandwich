2dcc3f2d4274d5f7fd67cb205b63ef02bb2537c2
varun <avarunsrisrimal@gmail.com>
2016-01-30 00:23:54 -0500

    Drag completed

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -3,43 +3,33 @@ using System.Collections;
 
 public class Ingredient : MonoBehaviour
 {
-    Ray ray;
-    Transform originalPosition;//original Position
-    Vector3 offset;
-
+    Vector3 originalPosition;//original Position
+    public float height;
     // Use this for initialization
     void Start()
     {
-        originalPosition = transform;
+        originalPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            if (SlingShotParent.obj != null)//if there is no obj already selected
-            {
-                SlingShotParent.obj = MousePick.GetObject(out ray);
-                if (SlingShotParent.obj != null)//if there is actually an obj
-                {
-                    if (SlingShotParent.obj.tag == gameObject.tag)
-                    {
-                        // gameObject.transform.position = sling.transform.position;
-                        //Vector3 vec = sling.transform.position;
-                        //vec.y += 2.0f;
-                        //gameObject.transform.position = vec;
-                        //gameObject.transform.parent = sling.transform
-                        offset = SlingShotParent.obj.transform.position - ray.origin;
-
-
-                    }
-                }
-            }
-            else if(SlingShotParent.obj.tag == tag)
-            {
-                transform.position = new Vector3(ray.origin.x + offset.x, transform.position.y, ray.origin.z + offset.z);
-            }
-        }
+       
+    }
+    public void Reset()
+    {
+        transform.position = originalPosition;
+    }
+  
+    void OnCollisionEnter(Collision other)
+    {
+        SlingShotParent.obj = null;
+        // Destroy(other);
+        Debug.Log("ok");
+        transform.position = other.gameObject.transform.position;
+        Vector3 vec = other.gameObject.transform.position;
+        vec.y += 2.0f;
+        gameObject.transform.position = vec;
+        gameObject.transform.parent = other.gameObject.transform;
     }
 }
