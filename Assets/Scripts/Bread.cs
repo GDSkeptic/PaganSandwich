075733333f089a04a536296db352a402eb24bb12
Derek Bliss <dbliss@fullsail.com>
2016-01-30 01:04:20 -0500

    Added bread and FurnaceCheck scripts

--- Assets/Scripts/Bread.cs
+++ Assets/Scripts/Bread.cs
@@ -1,9 +1,9 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Bread : MonoBehaviour {
 
-    public string[] RequestedIngredients;
+    public List<string> RequestedIngredients;
 
     public float BreadHeight = 1;
 
@@ -12,6 +12,7 @@ public class Bread : MonoBehaviour {
 
     void Start()
     {
+        RequestedIngredients = new List<string>(2);
         CurrentSandwichHeight = BreadHeight;
     }
 
@@ -20,11 +21,17 @@ public class Bread : MonoBehaviour {
         GameObject other = _collision.gameObject;
         if(other.tag == "Tomatoe" || other.tag == "Lettuce" || other.tag == "Steak" || other.tag == "Chicken")
         {
-            other.GetComponent<Rigidbody>().velocity = Vector3(0, 0, 0);
+            other.GetComponent<Rigidbody>().velocity = Vector3.zero;
             other.transform.rotation = Quaternion.Euler(0, 0, 0);
             other.transform.position = transform.position + Vector3.up * CurrentSandwichHeight;
             CurrentSandwichHeight += other.GetComponent<Ingredient>().height;
             other.transform.parent = transform;
         }
+    }
+
+    void OnTriggerEnter(Collider _trigger)
+    {
+        if(_trigger.tag == "Furnace")
+            Destroy(gameObject);
     }
 }
--- Assets/Scripts/FurnaceCheck.cs
+++ Assets/Scripts/FurnaceCheck.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class FurnaceCheck : MonoBehaviour {
 
+    public delegate void PunchSandwich();
+
+    public event PunchSandwich OnPunchSandwich;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -18,6 +22,30 @@ public class FurnaceCheck : MonoBehaviour {
         GameObject ObjectToTest = _other.gameObject;
         Bread bread = ObjectToTest.GetComponent<Bread>();
         Ingredient[] stuff = ObjectToTest.GetComponentsInChildren<Ingredient>();
+        if(bread.RequestedIngredients.Count != stuff.Length)
+        {
+            // bad sandwich!!!
+            if (OnPunchSandwich != null)
+                OnPunchSandwich();
+        }
+        foreach(Ingredient i in stuff)
+        {
+            bool found = false;
+            for (int s = 0; s < bread.RequestedIngredients.Count; ++s)
+            {
+                if (i.tag == bread.RequestedIngredients[s])
+                {
+                    found = true;
+                    bread.RequestedIngredients.RemoveAt(s);
+                    break;
+                }
+            }
+            if (!found && OnPunchSandwich != null)
+            {
+                OnPunchSandwich();
+                break;
+            }
+        }
     }
 
 }
