602c62fa2392f9d7c4bae2f2e6b0da756d8ba89f
Derek Bliss <dbliss@fullsail.com>
2016-01-30 00:20:55 -0500

    Bread and FurnaceCheck placeholders

---
+++ Assets/Scripts/Bread.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bread : MonoBehaviour {
+
+    public string[] RequestedIngredients;
+
+    public float BreadHeight = 1;
+
+    [SerializeField]
+    float CurrentSandwichHeight = 1;
+
+    void Start()
+    {
+        CurrentSandwichHeight = BreadHeight;
+    }
+
+    void OnCollisionEnter(Collision _collision)
+    {
+        GameObject other = _collision.gameObject;
+        if(other.tag == "Tomatoe" || other.tag == "Lettuce" || other.tag == "Steak" || other.tag == "Chicken")
+        {
+            other.GetComponent<Rigidbody>().velocity = Vector3(0, 0, 0);
+            other.transform.rotation = Quaternion.Euler(0, 0, 0);
+            other.transform.position = transform.position + Vector3.up * CurrentSandwichHeight;
+            CurrentSandwichHeight += other.GetComponent<Ingredient>().height;
+            other.transform.parent = transform;
+        }
+    }
+}
---
+++ Assets/Scripts/FurnaceCheck.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FurnaceCheck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider _other)
+    {
+        GameObject ObjectToTest = _other.gameObject;
+        Bread bread = ObjectToTest.GetComponent<Bread>();
+        Ingredient[] stuff = ObjectToTest.GetComponentsInChildren<Ingredient>();
+    }
+
+}
